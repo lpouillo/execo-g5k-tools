0937fe02fcbfd655db307da16dcd7ebf26ff1ad8
Laurent Pouilloux <lolo@pouilloux.org>
2014-08-27 12:50:09 +0200

    [engines] paasage_simu: fix indent

--- engines/simgrid_paasage/run_all_execo.py
+++ engines/simgrid_paasage/run_all_execo.py
@@ -31,8 +31,8 @@ vm_map_to_service={'0'     :'lb_http',
                    '5'     :'app',
                    '6'     :'app',
                    '7'     :'db',
-	           '8'     :'db'
-	          }
+               '8'     :'db'
+              }
 
 
 
@@ -45,46 +45,46 @@ def replaceWord(src, dst, word1, word2):
     outFile=open(dst, "w");
     for line in inFile.readlines():
         newLine=line.replace(word1,word2)
-	outFile.write(newLine); 
+    outFile.write(newLine); 
 
 def insertExp(expName, traceFile, xmlConfFile):
     try:
-	sql = "INSERT INTO exp (exp_name, exp_id_cfg, trace_file, xml_file) VALUES (%s, %s, %s, %s)"
-        cur.execute(sql,(expName, 1, traceFile, xmlConfFile))
-	exp_id=cur.lastrowid
-	db.commit()   
+        sql = "INSERT INTO exp (exp_name, exp_id_cfg, trace_file, xml_file) VALUES (%s, %s, %s, %s)"
+            cur.execute(sql,(expName, 1, traceFile, xmlConfFile))
+        exp_id=cur.lastrowid
+        db.commit()   
     except:
-	db.rollback()
+        db.rollback()
     return exp_id
 
-def updateVm(vm_name, tier_id, vm_cfg_id, exp_id):			
+def updateVm(vm_name, tier_id, vm_cfg_id, exp_id):            
     try:
-	sql="INSERT INTO vm (vm_name, tier_id, vm_cfg_id, exp_id ) VALUES (%s, %s, %s, %s)"
-	cur.execute(sql, (vm_name, tier_id, vm_cfg_id, exp_id))
-	vm_id=cur.lastrowid    
-	db.commit()   
+        sql="INSERT INTO vm (vm_name, tier_id, vm_cfg_id, exp_id ) VALUES (%s, %s, %s, %s)"
+        cur.execute(sql, (vm_name, tier_id, vm_cfg_id, exp_id))
+        vm_id=cur.lastrowid    
+        db.commit()   
     except:
-	db.rollback()
+        db.rollback()
     return vm_id
 
 def insertTrace(vm_id, start, final, duration, state, url):
     try:
-	sql = "INSERT INTO trace (vm_id, ts_start, ts_final, duration, state, url) VALUES (%s, %s, %s, %s, %s, %s)"		
-	cur.execute(sql,(vm_id, start, final, duration, state, url))
-	trace_id=cur.lastrowid
-	db.commit()   
+        sql = "INSERT INTO trace (vm_id, ts_start, ts_final, duration, state, url) VALUES (%s, %s, %s, %s, %s, %s)"        
+        cur.execute(sql,(vm_id, start, final, duration, state, url))
+        trace_id=cur.lastrowid
+        db.commit()   
     except:
-	db.rollback()
+        db.rollback()
     return trace_id
 
 def insertStatistic(rtt_global, rtt_lb_http, rtt_http, rtt_lb_app, rtt_app, rtt_lb_db, rtt_db, throughput, cost, exp_id):
     try:
-	sql = "INSERT INTO statistic (rtt_global, rtt_lb_http, rtt_http, rtt_lb_app, rtt_app, rtt_lb_db, rtt_db, throughput, cost, exp_id)       VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"		
-	cur.execute(sql,(rtt_global, rtt_lb_http, rtt_http, rtt_lb_app, rtt_app, rtt_lb_db, rtt_db, throughput, cost, exp_id))
-	stat_id=cur.lastrowid
-	db.commit()   
+        sql = "INSERT INTO statistic (rtt_global, rtt_lb_http, rtt_http, rtt_lb_app, rtt_app, rtt_lb_db, rtt_db, throughput, cost, exp_id)       VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"        
+        cur.execute(sql,(rtt_global, rtt_lb_http, rtt_http, rtt_lb_app, rtt_app, rtt_lb_db, rtt_db, throughput, cost, exp_id))
+        stat_id=cur.lastrowid
+        db.commit()   
     except:
-	db.rollback()
+        db.rollback()
     return stat_id
 
 def getId(id_col, table, col, value):
@@ -94,50 +94,49 @@ def getId(id_col, table, col, value):
 
 if __name__ == "__main__":
 
-	usage = "usage: %prog [options] [args] "
-	parser = OptionParser(usage=usage)
+    usage = "usage: %prog [options] [args] "
+    parser = OptionParser(usage=usage)
 
-	parser.add_option("-f", dest="confFile", help="experiments' configuration file name", default="conf.xml")
-	parser.add_option("--cb", dest="comb", help="current combination")
+    parser.add_option("-f", dest="confFile", help="experiments' configuration file name", default="conf.xml")
+    parser.add_option("--cb", dest="comb", help="current combination")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	if not (options.confFile):
-		parser.error("You must provide the configuration file name of the experiments !")
-
-
-	CONF=options.confFile
-	name=options.comb
-
-	if  not os.path.exists("log") :   
-		os.mkdir("/home/Work/sgcbntier/paasage_demo/log",755);
-	if  not os.path.exists("csv") :
-		os.mkdir("/home/Work/sgcbntier/paasage_demo/csv",755);
-	
-	
-	
-	traceFile="ntier_"+name
-	confFile="sgcb_ntier_"+name+".conf"
-	xmlConfFile="test.xml"
+    if not (options.confFile):
+        parser.error("You must provide the configuration file name of the experiments !")
 
 
-	shutil.copyfile("/home/Work/sgcbntier/paasage_demo/sgcb_ntier.conf", "/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp")
+    CONF=options.confFile
+    name=options.comb
 
-	replaceWord("/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp", "/home/Work/sgcbntier/paasage_demo/"+confFile,"TRACE_FILE",traceFile);
-	replaceWord("/home/Work/sgcbntier/paasage_demo/"+confFile, "/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp","XML_FILE",xmlConfFile);
+    if  not os.path.exists("log") :   
+        os.mkdir("/home/Work/sgcbntier/paasage_demo/log",755);
+    if  not os.path.exists("csv") :
+        os.mkdir("/home/Work/sgcbntier/paasage_demo/csv",755);
+    
+    
+    
+    traceFile="ntier_"+name
+    confFile="sgcb_ntier_"+name+".conf"
+    xmlConfFile="test.xml"
 
-	shutil.move("/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp", "/home/Work/sgcbntier/paasage_demo/"+confFile)
 
-		     
-	os.system(JVM+" -jar /home/Work/sgcbntier/paasage_demo/"+SGCBJAR+" 100 /home/Work/sgcbntier/paasage_demo/"+confFile+" >  /home/Work/sgcbntier/paasage_demo/log/"+traceFile+".log")
-	os.system(PJDUMP+" /home/Work/sgcbntier/paasage_demo/"+traceFile+".trace | grep REQTASK > /home/Work/sgcbntier/paasage_demo/csv/REQTASK_"+traceFile+".csv") 
+    shutil.copyfile("/home/Work/sgcbntier/paasage_demo/sgcb_ntier.conf", "/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp")
 
-	os.remove("/home/Work/sgcbntier/paasage_demo/"+traceFile+".trace")
-	os.remove("/home/Work/sgcbntier/paasage_demo/"+confFile)
-	os.remove("/home/Work/sgcbntier/paasage_demo/"+xmlConfFile)
-	os.remove("/home/Work/sgcbntier/paasage_demo/"+traceFile+".plist")
-	    
+    replaceWord("/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp", "/home/Work/sgcbntier/paasage_demo/"+confFile,"TRACE_FILE",traceFile);
+    replaceWord("/home/Work/sgcbntier/paasage_demo/"+confFile, "/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp","XML_FILE",xmlConfFile);
 
-               	
+    shutil.move("/home/Work/sgcbntier/paasage_demo/"+confFile+".tmp", "/home/Work/sgcbntier/paasage_demo/"+confFile)
 
+             
+    os.system(JVM+" -jar /home/Work/sgcbntier/paasage_demo/"+SGCBJAR+" 100 /home/Work/sgcbntier/paasage_demo/"+confFile+" >  /home/Work/sgcbntier/paasage_demo/log/"+traceFile+".log")
+    os.system(PJDUMP+" /home/Work/sgcbntier/paasage_demo/"+traceFile+".trace | grep REQTASK > /home/Work/sgcbntier/paasage_demo/csv/REQTASK_"+traceFile+".csv") 
+
+    os.remove("/home/Work/sgcbntier/paasage_demo/"+traceFile+".trace")
+    os.remove("/home/Work/sgcbntier/paasage_demo/"+confFile)
+    os.remove("/home/Work/sgcbntier/paasage_demo/"+xmlConfFile)
+    os.remove("/home/Work/sgcbntier/paasage_demo/"+traceFile+".plist")
+        
+
+                   
 
--- engines/simgrid_paasage/xml_gen_execo.py
+++ engines/simgrid_paasage/xml_gen_execo.py
@@ -6,163 +6,162 @@ import re
 import itertools
 
 from optparse import OptionParser
-	
+    
 
 def initXML():
-	
-	root = ET.Element("nTierApplication")
-	root.set("version", "1")
+    
+    root = ET.Element("nTierApplication")
+    root.set("version", "1")
 
-	ami = ET.SubElement(root, "AMI")
+    ami = ET.SubElement(root, "AMI")
 
-	field1 = ET.SubElement(ami, "webService")
-	field1.set("size", "10000000000.0")
-	field2 = ET.SubElement(ami, "appsService")
-	field2.set("size", "10000000000.0")
-	field3 = ET.SubElement(ami, "dbService")
-	field3.set("size", "10000000000.0")
-	field4 = ET.SubElement(ami, "webProxy")
-	field4.set("size", "10000000000.0")
-	field5 = ET.SubElement(ami, "appsProxy")
-	field5.set("size", "10000000000.0")
-	field6 = ET.SubElement(ami, "dbProxy")
-	field6.set("size", "10000000000.0")
+    field1 = ET.SubElement(ami, "webService")
+    field1.set("size", "10000000000.0")
+    field2 = ET.SubElement(ami, "appsService")
+    field2.set("size", "10000000000.0")
+    field3 = ET.SubElement(ami, "dbService")
+    field3.set("size", "10000000000.0")
+    field4 = ET.SubElement(ami, "webProxy")
+    field4.set("size", "10000000000.0")
+    field5 = ET.SubElement(ami, "appsProxy")
+    field5.set("size", "10000000000.0")
+    field6 = ET.SubElement(ami, "dbProxy")
+    field6.set("size", "10000000000.0")
 
-	proxy = ET.SubElement(root, "proxy")
+    proxy = ET.SubElement(root, "proxy")
 
-	field7 = ET.SubElement(proxy, "webProxy")
-	field7.set("region", "eu_1")
-	field7.set("instanceType","m1.small")
+    field7 = ET.SubElement(proxy, "webProxy")
+    field7.set("region", "eu_1")
+    field7.set("instanceType","m1.small")
 
-	field8 = ET.SubElement(proxy, "appsProxy")
-	field8.set("region", "eu_1")
-	field8.set("instanceType","m1.small")
+    field8 = ET.SubElement(proxy, "appsProxy")
+    field8.set("region", "eu_1")
+    field8.set("instanceType","m1.small")
 
-	field9 = ET.SubElement(proxy, "dbProxy")
-	field9.set("region", "eu_1")
-	field9.set("instanceType","m1.small")
+    field9 = ET.SubElement(proxy, "dbProxy")
+    field9.set("region", "eu_1")
+    field9.set("instanceType","m1.small")
 
 
-	return root
+    return root
 
 def createService(parent, name):
-	tmp = ET.SubElement(parent, name)
-	return tmp
+    tmp = ET.SubElement(parent, name)
+    return tmp
 
 def createRegion(parent, name):
-	tmp = ET.SubElement(parent, "region")
-	tmp.set("name", name )
-	return tmp
+    tmp = ET.SubElement(parent, "region")
+    tmp.set("name", name )
+    return tmp
 
 def createInstance(parent, ty, qt):
-	tmp = ET.SubElement(parent, "instance")
-	tmp.set("quantity", qt )
-	tmp.set("type", ty )
-	return tmp	
+    tmp = ET.SubElement(parent, "instance")
+    tmp.set("quantity", qt )
+    tmp.set("type", ty )
+    return tmp    
 
 
 def generateExp(lis, rootSrc):
-	root=initXML()
-	servParent=ET.SubElement(root, "services")
-	servWeb=createService(servParent,"webService")
-	servApp=createService(servParent,"appsService")
-	servDb=createService(servParent,"dbService")
+    root=initXML()
+    servParent=ET.SubElement(root, "services")
+    servWeb=createService(servParent,"webService")
+    servApp=createService(servParent,"appsService")
+    servDb=createService(servParent,"dbService")
 
-	i=0
+    i=0
 
-	web=rootSrc.find("webService")
-	if (web==None):
-		print "webService tag not found!"
-		exit(1)
-	for child1 in web.iter("region"):
-		regionTmp=createRegion(servWeb, child1.get("name"))
-		for child2 in child1.iter("instance"):	
-			if (lis[i] != '0'):
-				createInstance(regionTmp, child2.get("type"), lis[i])
-				i+=1
-			else:
-				i+=1
-				continue
-		if not regionTmp.getchildren():
-			servWeb.remove(regionTmp)
-	if(not servWeb.getchildren()):
-		print "ERROR: Web service does not has any vm instance associated for first experiment"
-		exit(2)		
-		
-	app=rootSrc.find("appsService")
-	if (app==None):
-		print "ERROR: appsService tag not found!"
-		exit(1)
-	for child1 in app.iter("region"):
-		regionTmp=createRegion(servApp, child1.get("name"))
-		for child2 in child1.iter("instance"):		
-			if (lis[i] != '0'):
-				createInstance(regionTmp, child2.get("type"), lis[i])
-				i+=1
-			else:
-				i+=1
-				continue
-		if not regionTmp.getchildren():
-			servApp.remove(regionTmp)
-	if(not servApp.getchildren()):
-		print "ERROR: Apps Service does not has any vm instance associated for first experiment"
-		exit(2)
-		
-			
+    web=rootSrc.find("webService")
+    if (web==None):
+        print "webService tag not found!"
+        exit(1)
+    for child1 in web.iter("region"):
+        regionTmp=createRegion(servWeb, child1.get("name"))
+        for child2 in child1.iter("instance"):    
+            if (lis[i] != '0'):
+                createInstance(regionTmp, child2.get("type"), lis[i])
+                i+=1
+            else:
+                i+=1
+                continue
+        if not regionTmp.getchildren():
+            servWeb.remove(regionTmp)
+    if(not servWeb.getchildren()):
+        print "ERROR: Web service does not has any vm instance associated for first experiment"
+        exit(2)        
+        
+    app=rootSrc.find("appsService")
+    if (app==None):
+        print "ERROR: appsService tag not found!"
+        exit(1)
+    for child1 in app.iter("region"):
+        regionTmp=createRegion(servApp, child1.get("name"))
+        for child2 in child1.iter("instance"):        
+            if (lis[i] != '0'):
+                createInstance(regionTmp, child2.get("type"), lis[i])
+                i+=1
+            else:
+                i+=1
+                continue
+        if not regionTmp.getchildren():
+            servApp.remove(regionTmp)
+    if(not servApp.getchildren()):
+        print "ERROR: Apps Service does not has any vm instance associated for first experiment"
+        exit(2)
+        
+            
 
-	db=rootSrc.find("dbService")
-	if (db==None):
-		print "ERROR: dbService tag not found!"
-		exit(1)
-	for child1 in db.iter("region"):
-		regionTmp=createRegion(servDb, child1.get("name"))
-		for child2 in child1.iter("instance"):	
-			if (lis[i] != '0'):
-				createInstance(regionTmp, child2.get("type"), lis[i])
-				i+=1
-			else:
-				i+=1
-				continue
-		if not regionTmp.getchildren():	
-   			servDb.remove(regionTmp)
-	if(not servDb.getchildren()):
-		print "ERROR: Db service does not has any vm instance associated for first experiment"
-		exit(2)		
+    db=rootSrc.find("dbService")
+    if (db==None):
+        print "ERROR: dbService tag not found!"
+        exit(1)
+    for child1 in db.iter("region"):
+        regionTmp=createRegion(servDb, child1.get("name"))
+        for child2 in child1.iter("instance"):    
+            if (lis[i] != '0'):
+                createInstance(regionTmp, child2.get("type"), lis[i])
+                i+=1
+            else:
+                i+=1
+                continue
+        if not regionTmp.getchildren():    
+               servDb.remove(regionTmp)
+    if(not servDb.getchildren()):
+        print "ERROR: Db service does not has any vm instance associated for first experiment"
+        exit(2)        
 
 
-	xml_string=ET.tostring(root, encoding='utf8', method='xml')
-	xml = DOM.parseString(xml_string) 
-	pretty_xml_as_string = xml.toprettyxml()
-	outFile=open("/home/Work/sgcbntier/paasage_demo/test.xml", "w")
-	outFile.write(pretty_xml_as_string)
+    xml_string=ET.tostring(root, encoding='utf8', method='xml')
+    xml = DOM.parseString(xml_string) 
+    pretty_xml_as_string = xml.toprettyxml()
+    outFile=open("/home/Work/sgcbntier/paasage_demo/test.xml", "w")
+    outFile.write(pretty_xml_as_string)
 
 
 
 
 if __name__ == "__main__":
 
-	
-	tree = ET.parse("/home/Work/sgcbntier/paasage_demo/conf.xml")
-	rootSrc = tree.getroot()
+    
+    tree = ET.parse("/home/Work/sgcbntier/paasage_demo/conf.xml")
+    rootSrc = tree.getroot()
 
 
-	usage = "usage: %prog [options] [args] "
-	parser = OptionParser(usage=usage)
+    usage = "usage: %prog [options] [args] "
+    parser = OptionParser(usage=usage)
 
-	parser.add_option("--cb", dest="comb", help="current combination")
+    parser.add_option("--cb", dest="comb", help="current combination")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	if not (options.comb):
-		parser.error("You must provide parameters for the experiment !")
+    if not (options.comb):
+        parser.error("You must provide parameters for the experiment !")
 
 
-	comb=options.comb
+    comb=options.comb
 
-	comb_list=comb.split("_")
+    comb_list=comb.split("_")
 
-	generateExp(comb_list, rootSrc) 
-	
-
+    generateExp(comb_list, rootSrc) 
+    
 
 
